5e444452024c56a5526f9bbebf8303112eb98164
xjoaoxsousax <joao.sousa4@gmail.com>
2025-04-13 16:26:34 +0100

    Adiciona build para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rotas/', // Substitua pelo nome do repositório no GitHub Pages
+  base: '/rotas-gitpage/', // Substitua pelo nome do repositório no GitHub Pages
 });
